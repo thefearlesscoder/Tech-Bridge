33d528c7da4c737dcc5135676223517abdecb0d9
thefearlesscoder <thefunnelvkd@gmail.com>
2025-04-18 21:12:08 +0530

    Set up clear client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,8 @@ import App from './App.jsx'
 const clerkFrontendApi = import.meta.env.VITE_CLERK_FRONTEND_API;
 
 createRoot(document.getElementById('root')).render(
-
-  <ClerkProvider frontendApi={clerkFrontendApi}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ClerkProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 
 )

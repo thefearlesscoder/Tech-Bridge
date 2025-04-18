48e421715ea874d008bdcb597afc4672aef2cd6e
Kunalsonkar07 <sonkarkunal812@gmail.com>
2025-04-19 00:03:10 +0530

    project upload done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar';
 import { SignIn, SignUp } from '@clerk/clerk-react'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
+import SignupForm from './pages/SignupForm';
+import LoginForm from './pages/LoginForm';
+import CreateProjectPage from './pages/UploadPorject';
 
 function App() {
   return (
@@ -12,8 +15,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home /> }/>
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/signin" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/upload-project" element={< CreateProjectPage/>} />
           </Routes>
       </div>
   );

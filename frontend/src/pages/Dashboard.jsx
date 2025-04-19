e6a6c8b7c57c3abfdbe8751ed241c452e6b15bc3
Varun <varunsahu10109@gmail.com>
2025-04-19 21:02:52 +0530

    some changes in registration

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
-TopProjects
+// TopProjects
 import Banner from "../components/Banner/Banner";
 import Subscribe from "../components/Subscribe/Subscribe";
 import Testimonials from "../components/Testimonials/Testimonials";
